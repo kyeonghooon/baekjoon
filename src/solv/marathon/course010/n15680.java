596f241940425324fcea3dcb08dd63d8079f525e
kyeonghooon <gibbon0510@gmail.com>
2024-08-07 09:22:54 +0900

    Maraton course 10 (3/10)

---
+++ src/solv/marathon/course010/n15680.java
@@ -0,0 +1,25 @@
+package solv.marathon.course010;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class n15680 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n1 = Integer.parseInt(br.readLine());
+		br.close();
+		if (n1 == 0) {
+			bw.write("YONSEI");
+		} else {
+			bw.write("Leading the Way to the Future");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
