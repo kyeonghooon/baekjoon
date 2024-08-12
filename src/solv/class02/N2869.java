f01e801edc1df561734b2c79b6eaf6bb0bea7a2e
kyeonghooon <gibbon0510@gmail.com>
2024-08-13 08:36:29 +0900

    class 2 달팽이 ~~

---
+++ src/solv/class02/N2869.java
@@ -0,0 +1,19 @@
+package solv.class02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N2869 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(token.nextToken());
+		int B = Integer.parseInt(token.nextToken());
+		int V = Integer.parseInt(token.nextToken());
+		System.out.println((int)Math.ceil((double)(V - A) / (A - B)) + 1);
+	}
+
+}
