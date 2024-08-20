e7735d082c375be2f3ccfc09402e4c73c1ef19d5
kyeonghooon <gibbon0510@gmail.com>
2024-08-20 20:59:58 +0900

    Maraton course 11 (6/8)

---
+++ src/solv/marathon/course011/N17356.java
@@ -0,0 +1,21 @@
+package solv.marathon.course011;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N17356 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		br.close();
+		int A = Integer.parseInt(token.nextToken());
+		int B = Integer.parseInt(token.nextToken());
+		double M = (double)(B - A) / 400;
+		double result = 1 / (1 + Math.pow(10, M));
+		System.out.println(result);
+	}
+
+}
