77db3257979c4aa5bb4556b4de6e0f4b6ae4cc64
kyeonghooon <gibbon0510@gmail.com>
2024-08-25 17:52:52 +0900

    class 2 : 좌표 정렬 1,2

---
+++ src/solv/class02/CoordinateAlignment2.java
@@ -0,0 +1,44 @@
+package solv.class02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CoordinateAlignment2 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int count = Integer.parseInt(br.readLine());
+        int[][] points = new int[count][2];
+        
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(points, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(points[i][0]).append(" ").append(points[i][1]).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
