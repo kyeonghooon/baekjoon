c578a3d34250ed205291a29e949a0bb72d351896
kyeonghooon <gibbon0510@gmail.com>
2024-08-26 09:01:11 +0900

    class 2 : 설탕 배달

---
+++ src/solv/class02/SugarDelivery.java
@@ -0,0 +1,26 @@
+package solv.class02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SugarDelivery {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(function(N));
+	}
+	public static int function(int N) {
+		int div5 = N / 5;
+		for (int i = div5; i >= 0; i--) {
+			for (int j = (N - i * 5) / 3; j >= 0; j--) {
+				if ((i * 5 + j * 3) == N) {
+					return i + j;
+				}
+			}
+		}
+		return -1;
+	}
+
+}
