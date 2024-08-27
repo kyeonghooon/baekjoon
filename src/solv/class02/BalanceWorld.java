e3965253e5eaf480a189ada2fc168589677804ac
kyeonghooon <gibbon0510@gmail.com>
2024-08-27 19:45:07 +0900

    class 2 : 균형잡힌 세상

---
+++ src/solv/class02/BalanceWorld.java
@@ -0,0 +1,51 @@
+package solv.class02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class BalanceWorld {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			String str = br.readLine();
+			if (str.equals(".")) {
+				break;
+			}
+			if (isBalance(str)) {
+				sb.append("yes\n");
+			} else {
+				sb.append("no\n");
+			}
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	public static boolean isBalance(String str) {
+		Stack<Character> stack = new Stack<>();
+		for (char ch : str.toCharArray()) {
+			if (ch == '(' || ch == '[') {
+				stack.push(ch);
+			} else if (ch == ')') {
+				if (stack.isEmpty() || stack.peek() != '(') {
+					return false;
+				} 
+				stack.pop();
+			} else if (ch == ']') {
+				if (stack.isEmpty() || stack.peek() != '[') {
+					return false;
+				} 
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+}
