9a6f3685f6cd4a1fee2c404a91d18ea589f10da4
kyeonghooon <gibbon0510@gmail.com>
2024-08-28 08:15:32 +0900

    Maraton course 13 (2/8)

---
+++ src/solv/marathon/course13/N5338.java
@@ -0,0 +1,15 @@
+package solv.marathon.course13;
+
+public class N5338 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+		System.out.println(sb);
+	}
+
+}
