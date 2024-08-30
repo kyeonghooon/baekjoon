e3a0baf03daa28b3e059d2b0e3bc09542c0e516a
kyeonghooon <gibbon0510@gmail.com>
2024-08-30 09:03:06 +0900

    Maraton course 13 (5/8)

---
+++ src/solv/marathon/course013/N17388.java
@@ -0,0 +1,30 @@
+package solv.marathon.course013;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N17388 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int S = Integer.parseInt(token.nextToken());
+		int K = Integer.parseInt(token.nextToken());
+		int H = Integer.parseInt(token.nextToken());
+		if (S + K + H >= 100) {
+			System.out.println("OK");
+		} else {
+			int min = Math.min(Math.min(S, K), H);
+			if (min == S) {
+				System.out.println("Soongsil");
+			} else if (min == K) {
+				System.out.println("Korea");
+			} else {
+				System.out.println("Hanyang");
+			}
+		}
+	}
+
+}
---
+++ src/solv/marathon/course013/N28691.java
@@ -0,0 +1,31 @@
+package solv.marathon.course013;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N28691 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		switch (str) {
+		case "M":
+			System.out.println("MatKor");
+			break;
+		case "W":
+			System.out.println("WiCys");
+			break;
+		case "C":
+			System.out.println("CyKor");
+			break;
+		case "A":
+			System.out.println("AlKor");
+			break;
+		case "$":
+			System.out.println("$clear");
+			break;
+		}
+	}
+
+}
