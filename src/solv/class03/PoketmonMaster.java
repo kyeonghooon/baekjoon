bd9e56836e67adda5c7b7d6d7369014878c971f5
kyeonghooon <gibbon0510@gmail.com>
2024-09-08 16:50:35 +0900

    class 3 : 나는 야 포켓몬마스터

---
+++ src/solv/class03/PoketmonMaster.java
@@ -0,0 +1,35 @@
+package solv.class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PoketmonMaster {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] nums = br.readLine().split(" ");
+		int N = Integer.parseInt(nums[0]);
+		int M = Integer.parseInt(nums[1]);
+		Map<String, Integer> map = new HashMap<>();
+		String[] names = new String[N];
+		for (int i = 0; i < N; i++) {
+			String name = br.readLine();
+			names[i] = name;
+			map.put(name, i + 1);
+		}
+		for (int i = 0; i < M; i++) {
+			String q = br.readLine();
+			if (q.matches("^[0-9]+$")) {
+				sb.append(names[Integer.parseInt(q) - 1]).append("\n");
+			} else {
+				sb.append((map.get(q))).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
