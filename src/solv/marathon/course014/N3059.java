52303f5e1108f109a7295a5c2aee278c6968e74f
kyeonghooon <gibbon0510@gmail.com>
2024-09-14 16:20:04 +0900

    Maraton course 15 (5/8)

---
+++ src/solv/marathon/course014/N3059.java
@@ -0,0 +1,26 @@
+package solv.marathon.course014;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N3059 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			String s = br.readLine();
+			for (int j = 'A'; j <= 'Z'; j++) {
+				if (s.indexOf(j) == -1) {
+					sum += j;
+				}
+			}
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
