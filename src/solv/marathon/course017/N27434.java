05a8d5b400331250c5ebd5809206c24fa081b943
kyeonghooon <gibbon0510@gmail.com>
2024-09-25 14:03:15 +0900

    Maraton course 17 (2/8)

--- src/solv/marathon/course017/N27434.java
+++ src/solv/marathon/course017/N27434.java
@@ -6,19 +6,20 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class N27434 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		br.close();
-		if (N == 0) {
-			System.out.println(1);
-			return;
+		System.out.println(factorial(1, N));
+	}
+
+	public static BigInteger factorial(int a, int n) {
+		BigInteger num = BigInteger.valueOf(a);
+		if (a < n) {
+			int b = (a + n) / 2;
+			num = factorial(a, b).multiply(factorial(b + 1, n));
 		}
-		BigInteger result = new BigInteger("1");
-		for (int i = 1; i <= N; i++) {
-			result = result.multiply(new BigInteger(i + ""));
-		}
-		System.out.println(result.toString());
+		return num;
 	}
 }
