f8df6fca2952ecd0761dbe16dfc8c27b7f2b6beb
kyeonghooon <gibbon0510@gmail.com>
2024-09-26 17:46:26 +0900

    Programmers Lv2 : 최소값 만들기

---
+++ src/solv/programmers/lv2/MakeMin.java
@@ -0,0 +1,23 @@
+package solv.programmers.lv2;
+
+import java.util.Arrays;
+
+public class MakeMin {
+
+	public static void main(String[] args) {
+		int[] A = new int[] { 1, 2 };
+		int[] B = new int[] { 3, 4 };
+		System.out.println(solution(A, B));
+	}
+
+	public static int solution(int[] A, int[] B) {
+		int answer = 0;
+		Arrays.sort(A);
+		Arrays.sort(B);
+		for (int i = 0; i < B.length; i++) {
+			answer += A[i] * B[B.length - 1 - i];
+		}
+		return answer;
+	}
+
+}
