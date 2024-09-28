20a4b19ecac667dcbff847c0fa7b4091cae2556e
kyeonghooon <gibbon0510@gmail.com>
2024-09-28 16:57:45 +0900

    Programmers Lv2 : JadenCase 문자열 만들기

---
+++ src/solv/programmers/lv2/JadenCaseString.java
@@ -0,0 +1,31 @@
+package solv.programmers.lv2;
+
+public class JadenCaseString {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String solution(String s) {
+		String answer = "";
+		boolean check = true;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == ' ') {
+				check = true;
+				sb.append(c);
+				continue;
+			}
+			if (check && 'a' <= c && c <= 'z') {
+				c -= 32;
+			} else if (!check && 'A' <= c && c <= 'Z') {
+                c += 32;
+            }
+			sb.append(c);
+			check = false;
+		}
+		answer = sb.toString();
+		return answer;
+	}
+}
