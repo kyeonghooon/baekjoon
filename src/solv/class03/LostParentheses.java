0021efee2610733c1b3816e0c0aedbcd65594652
kyeonghooon <gibbon0510@gmail.com>
2024-10-03 14:23:22 +0900

    class 3 : 잃어버린 괄호

---
+++ src/solv/class03/LostParentheses.java
@@ -0,0 +1,33 @@
+package solv.class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LostParentheses {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int sum = 0;
+		if (str.indexOf('-') > 0) {
+			String plusStr = str.substring(0, str.indexOf('-'));
+			String minusStr = str.substring(str.indexOf('-') + 1);
+			String[] plusNumStr = plusStr.split("[-+]");
+			String[] minusNumStr = minusStr.split("[-+]");
+			for (int i = 0; i < plusNumStr.length; i++) {
+				sum += Integer.parseInt(plusNumStr[i]);
+			}
+			for (int i = 0; i < minusNumStr.length; i++) {
+				sum -= Integer.parseInt(minusNumStr[i]);
+			}
+		} else {
+			String[] plusNumStr = str.split("[-+]");
+			for (int i = 0; i < plusNumStr.length; i++) {
+				sum += Integer.parseInt(plusNumStr[i]);
+			}
+		}
+		System.out.println(sum);
+	}
+	
+}
