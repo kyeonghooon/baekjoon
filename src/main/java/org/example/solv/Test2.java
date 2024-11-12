0dfadaf270c791a7fbd6bdca90c537cfec200575
kyeonghooon <gibbon0510@gmail.com>
2024-11-12 10:40:36 +0900

    Maraton course 23 (7/8)

--- src/main/java/org/example/solv/Test2.java
+++ src/main/java/org/example/solv/Test2.java
@@ -4,15 +4,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Timestamp;
 import java.time.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Test2 {
 
 
 	public static void main(String[] args) {
-		System.out.println(-1 % 12);
+		solution(2);
+	}
+
+	public static int[][] solution(int n) {
+		int[][] answer = {};
+		Stack<Integer> stack1 = new Stack<>();
+		for (int i = n; i > 0; i--) {
+			stack1.push(i);
+		}
+		Stack<Integer> stack2 = new Stack<>();
+		Stack<Integer> stack3 = new Stack<>();
+		while (!stack1.isEmpty() || !stack2.isEmpty()) {
+			if (!stack1.isEmpty()) {
+				int temp = stack1.pop();
+			}
+		}
+		return answer;
 	}
 }
---
+++ src/main/java/org/example/solv/marathon/course023/N9272.java
@@ -0,0 +1,20 @@
+package org.example.solv.marathon.course023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N9272 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int k = n2 - n1 + 1;
+        System.out.println(k * (k - 1) / 2);
+    }
+}
+
