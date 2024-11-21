300c8b9a73728e4ddf087c718bd907125d0a8af8
kyeonghooon <gibbon0510@gmail.com>
2024-11-21 09:34:32 +0900

    Programmers Lv2 : 이진 변환 반복하기

---
+++ src/main/java/org/example/solv/programmers/lv2/RepeatingBinaryConversion.java
@@ -0,0 +1,32 @@
+package org.example.solv.programmers.lv2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RepeatingBinaryConversion {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] result = solution(br.readLine());
+        System.out.println("[" + result[0] + "," + result[1] + "]");
+    }
+
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+        while (!s.equals("1")) {
+            String decStr = binaryConversion(s, answer);
+            s = Integer.toBinaryString(decStr.length());
+        }
+        return answer;
+    }
+
+    public static String binaryConversion(String s, int[] answer) {
+        String newStr = s.replace("0", "");
+        int zeroCount = s.length() - newStr.length();
+        answer[0]++;
+        answer[1] += zeroCount;
+        return newStr;
+    }
+
+}
